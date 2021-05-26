0de51471a4baeccb2444e5e270d680e3a95c9cbc
Katja Ruebel <Katja.Ruebel@gmail.com>
2021-05-26 23:45:50 +0200

    Added some examples in konvergenz.jl

---
+++ konvergenz.jl
@@ -0,0 +1,21 @@
+using SparseArrays
+using LinearAlgebra
+using Plots
+
+include("vekit.jl")
+
+let
+    A=[23 4 3; 5 4 5; -30 -10 -10]
+    B=[-1 0 1; 0 1 0; 1 0 -1]
+    C=[2 -3 1; 3 1 3; -5 2 -4]
+    y=[8; 3; 5]/norm([8; 3; 5])
+    e=10^-10
+    l_A,y_A=vekit(A,y,e)
+    f_A=map(x -> abs((x-maximum(eigen(A).values))/maximum(eigen(A).values)), l_A)
+    l_B,y_B=vekit(B,y,e)
+    f_B=map(x -> abs((x-maximum(eigen(B).values))/maximum(eigen(B).values)), l_B)
+    l_C,y_C=vekit(C,y,e)
+    f_C=map(x -> abs((x-maximum(eigen(C).values))/maximum(eigen(C).values)), l_C)
+
+    #TODO: Plot
+end
--- vekit.jl
+++ vekit.jl
@@ -3,20 +3,21 @@ using LinearAlgebra
 using Plots
 
 function vekit(A, y, e)
-    l=0;
+    l=[0];
     while true
         # do stuff
         y_h = A*y
-        l = dot(y_h,y)
+        l_h = dot(y_h,y)
+        l=[l;l_h]
         y=y_h/norm(y_h)
-        if (norm(A*y-l*y)<e)
-            return l,y,eigen(A).values;
+        if (norm(A*y-l_h*y)<e)
+            return l,y;
         end
     end
 end
 
 #print("rr")
-A = [1 2; 3 4]
-y = [1; 2]
-e = 0.1
-print(vekit(A,y,e))
+#A = [1 2; 3 4]
+#y = [1; 2]
+#e = 0.1
+#print(vekit(A,y,e))
