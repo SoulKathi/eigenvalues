e2b4f8464c44f6e1a98519a8e52c701f4f917375
Katja Ruebel <Katja.Ruebel@gmail.com>
2021-05-27 22:42:32 +0200

    implemented algorithm

--- invvekit.jl
+++ invvekit.jl
@@ -1,3 +1,26 @@
 using SparseArrays
 using LinearAlgebra
 using Plots
+
+function invvekit(A, y, m, e)
+    l=[0];
+    Q,R,p = lu(A-m*Diagonal(ones(size(A))))
+    while true
+        # do stuff
+        y_h=R\(Q' * y)
+        l_h = 1/(dot(y_h,y)) + m
+        l=[l;l_h]
+        y=y_h/norm(y_h)
+        print(norm(A*y-l_h*y))
+        if (norm(A*y-l_h*y)<e)
+            return l,y;
+        end
+    end
+end
+
+print("rr")
+A = [1 2; 3 4]
+y = [1; 2]
+m = 0
+e = 1
+print(invvekit(A,y,m,e))
